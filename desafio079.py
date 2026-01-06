c7cc47fc28bf58fbfc96bf4b32d5dadf88a18909
Felix Lima <32372236+felixlimaa@users.noreply.github.com>
2026-01-06 14:04:17 -0300

    Desafio079 atualizado e funcional
    Correção de código

--- desafio079.py
+++ desafio079.py
@@ -2,25 +2,23 @@ valores = ordem = []
 i = 0
 c = ""
 while True:
-    append.valores(str(input("Digite um número: ").strip()[i])
+    valores.append(str(input("Digite um número: ").strip()))
     while not valores[i].isnumeric:
-        append.valores(str(input("Valor inválido! Digite um número: ").strip()[i])
+        valores.append(str(input("Valor inválido! Digite um número: ").strip()))
+    if valores[i] in valores[:i]: #nao repete  valor inserido dentro da lista 
+        valores.pop(i)
     valores[i] = int(valores[i])
-    while valores[i] in valores[i]: #nao repete  valor inserido dentro da lista 
-        pop(valores[i])
-    
     c = str(input("Você quer adicionar mais algum número? [S/N]: ")).strip().upper()[0]
     while c not in "NS":
         c = str(input("Você quer adicionar mais algum número? [S/N]: ")).strip().upper()[0]
-    while "NS" in c:
-        if "N" in c:
-            break
+    if "N" in c:
+        break
     i += 1
 print(f"Você digitou os números: ", end=" ")
 for v in valores:
     print(v, end=" ")
-ordem = valores.sort()
+ordem = sorted(valores, reverse=False)
 print(f"\nOrdem crescente dos números: ", end=" ")
 for o in ordem:
     print(o, end=" ")
-print("Programa encerrado")
+print("\nPrograma encerrado")
