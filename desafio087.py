678a1c05417982d42e1a0bb9e81c4c575c8ccd53
Felix Lima <32372236+felixlimaa@users.noreply.github.com>
2026-01-10 10:58:47 -0300

    desafio087.py resolvido

---
+++ desafio087.py
@@ -0,0 +1,20 @@
+temp = []
+n = []
+for i in range(0,3):
+    for j in range(0,3):
+        temp.append(int(input(f"Digite um valor para {[i, j]}: ")))
+        if j == 2:
+            n.append(temp[:])
+            temp.clear()
+for i in range(0,3):
+    for j in range(0,3):
+        print(f"[ {n[i][j]} ]", end=" ")
+        if j == 2:
+            print()
+pares = [j for p in n for j in p if j%2 == 0]
+pares = sum(pares)
+colunaDois = [p[2] for p in n]
+colunaDois = sum(colunaDois)
+print(f"A soma dos valores pares é {pares}.")
+print(f"A soma dos valores da terceira coluna é {colunaDois}.")
+print(f"O maior valor da segunda linha é {max(n[1])}.")
