83f4849375a4d83d6c67a850df61e41c4e470fa9
Felix Lima <32372236+felixlimaa@users.noreply.github.com>
2026-01-16 19:25:28 -0300

    desafio093.py tá indo

--- desafio093.py
+++ desafio093.py
@@ -1,1 +1,6 @@
 jogador = {}
+jogador['nome'] = str(input()).strip().capitalize()
+jogador['partidas'] = int(f'Quantas partidas {jogador["nome"]} jogou? ')
+for p in range(0,jogador['partidas']):
+    jogador['gols'] = int(input(f"Quantos gols {jogador['nome']} fez na partida {p}? "))
+print(jogador)
