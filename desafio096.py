37d9623a42ad56fe5b10e00e04220a0c15a0943f
Felix Lima <32372236+felixlimaa@users.noreply.github.com>
2026-01-19 14:48:43 -0300

    desafio096.py resolvido

---
+++ desafio096.py
@@ -0,0 +1,9 @@
+def area(largura, altura):
+    print(f"Controle de Terrenos")
+    print("-"*(len("Controle de Terrenos")+4))
+    print(f"LARGURA (m): {largura}")
+    print(f"COMPRIMENTO (m): {altura}")
+    print(f"A área de um terreno {largura} x {altura} é de {largura*altura} m².")
+
+
+area(4.5, 8)
